94895c91e27e2f11e58de8dff0a137e512955916
Aditya Kumar Singh <168503964+adityaprocoders@users.noreply.github.com>
2026-02-03 14:26:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    FIREBASE_CONFIG: {
+    apiKey: "AIzaSyBaupaSGmYp8sZDRVYQVcF97Llk2vhtkbw",
+    authDomain: "examwarmup-2dc96.firebaseapp.com",
+    databaseURL: "https://examwarmup-2dc96-default-rtdb.firebaseio.com",
+    projectId: "examwarmup-2dc96",
+    storageBucket: "examwarmup-2dc96.firebasestorage.app",
+    messagingSenderId: "908042316536",
+    appId: "1:908042316536:web:3954d85b7810177edda90d"
+  },
+  OWNER_HASH:"f47860f2ce27befb830adb548967783cb230e273cdc950e7b45896c1ac8f24ba"
+};
